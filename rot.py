d53c252a67f3a0622b3808852037b3d9c0c1e686
lvsl <chin@shaftsoft.ru>
2011-12-14 21:24:16 +0600

    add file extension

---
+++ rot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python -u
+# coding: utf-8
+
+#
+# R.O.T - Rotate Output To
+#
+
+# XXX: -u turns off std{out,err,in} buffering
+# and set binary mode (see man page for details)
+
+def main():
+
+
